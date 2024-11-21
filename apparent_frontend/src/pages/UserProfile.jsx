e17eaf528d5999adb557f1a905b214be12d1c762
Jacob Burritt <u1046433@utah.edu>
2024-11-21 13:56:03 -0700

    Created a very basic user profile page with as of yet no backend support -- so far it just lets you change your avatar (displaying of which is yet to be implemented), display name, username (yet to have restrictions added to it, e.g., no spaces), and about me

--- apparent_frontend/src/pages/UserProfile.jsx
+++ apparent_frontend/src/pages/UserProfile.jsx
@@ -1,7 +1,45 @@
+function AvatarEditor() {
+    return(
+        <div>
+            Avatar: <input type="file" accept="image/*"/>
+        </div>
+    )
+}
+
+function DisplayNameEditor() {
+    return(
+        <div>
+            Display name: <input type="text" name="display name"/>
+        </div>
+    )
+}
+
+function UsernameEditor() {
+    return(
+        <div>
+            Username: @<input type="text" name="username"/>
+        </div>
+    )
+}
+
+function AboutMeEditor() {
+    return(
+        <div>
+            About me: <textarea name="about me" wid="500"/>
+        </div>
+    )
+}
+
 export function UserProfile() {
     return(
         <>
             <h1>Profile Page</h1>
+            <div/>
+
+            {AvatarEditor()}
+            {DisplayNameEditor()}
+            {UsernameEditor()}
+            {AboutMeEditor()}
         </>
     )
 }
