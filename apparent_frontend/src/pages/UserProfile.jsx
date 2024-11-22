d3b15d7e58505dd80f97ea84bb3c56444ac9dc09
Jacob Burritt <u1046433@utah.edu>
2024-11-22 09:11:09 -0700

    Added email field to the user profile page, also made it look slightly nicer, still no backend support yet though

--- apparent_frontend/src/pages/UserProfile.jsx
+++ apparent_frontend/src/pages/UserProfile.jsx
@@ -7,34 +7,62 @@ function AvatarEditor() {
     }
 
     return(
-        <div>
-            <div style={{width: 200, height: 200, border: "2px solid white"}}>
+        <div style={{margin: 10, border: "2px solid gray", padding: 10}}>
+            Avatar:
+            <span style={{width: 200, height: 200, border: "2px solid white", display: "inline-block"}}>
                 <img src={file} style={{maxWidth: "100%", maxHeight: "100%", scale: "auto"}}></img>
+            </span>
+            <div>
+                <input type="file" accept="image/*" onChange={handleAvatarFileChosen}/>
             </div>
-            Avatar: <input type="file" accept="image/*" onChange={handleAvatarFileChosen}/>
+        </div>
+    )
+}
+
+function EmailAddressEditor() {
+    
+    return(
+        <div style={{border: "2px solid gray", padding: 10}}>
+            Email: <input type="email" name="email"/>
         </div>
     )
 }
 
 function DisplayNameEditor() {
     return(
-        <div>
+        <div style={{border: "2px solid gray", padding: 10}}>
             Display name: <input type="text" name="display name"/>
         </div>
     )
 }
 
 function UsernameEditor() {
+    const [isUsernameValid, setIsUsernameValid] = useState('');
+    function handleUsernameChanged(e) {
+        var username = e.target.value;
+
+        setIsUsernameValid(testUsernameValidity(username));
+    }
+
+    function testUsernameValidity(username) {
+        var regex = new RegExp("^[A-Za-z0-9_]+$");
+        return regex.test(username);
+    }
+    
     return(
-        <div>
-            Username: @<input type="text" name="username"/>
+        <div style={{border: "2px solid gray", padding: 10}}>
+            Username: @<input type="text" name="username" onChange={handleUsernameChanged}/>
+            
+            <div style={{color: (isUsernameValid ? "#00ff00" : "#ff0000")}}>
+                 {(isUsernameValid ? "✓" : "✗")} Must be 1 or more alphanumeric characters and/or underscores
+            </div>
         </div>
     )
 }
 
 function AboutMeEditor() {
     return(
-        <div>
+        <div style={{border: "2px solid gray", padding: 10}}>
             About me: <textarea name="about me" rows="5" cols="40"/>
         </div>
     )
@@ -55,6 +83,7 @@ export function UserProfile() {
             <div/>
 
             {AvatarEditor()}
+            {EmailAddressEditor()}
             {DisplayNameEditor()}
             {UsernameEditor()}
             {AboutMeEditor()}
