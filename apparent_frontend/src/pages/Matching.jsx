56a700070f58dd6639e3bb4d728f50ee3523490f
Jacob <u1046433@utah.edu>
2024-12-06 08:50:41 -0700

    Started ProposedMatchCard

--- apparent_frontend/src/pages/Matching.jsx
+++ apparent_frontend/src/pages/Matching.jsx
@@ -6,6 +6,28 @@ function MatchPercentage(arg, target, min, max) {
     return 1 - Math.abs(target - arg) / Math.abs(target);
 }
 
+function ProposedMatchCard(name, age, description, kidAge1, kidAge2, kidAge3) {
+    return(
+        <>
+            <h1>
+            {name}
+            </h1>
+            <div>
+                Age: {age}
+            </div>
+                {kidAge1 == "" ? "" : "Child 1 age: "} {kidAge1 == "" ? "" : {kidAge1}}
+                {kidAge2 == "" ? "" : "Child 2 age: "} {kidAge2 == "" ? "" : {kidAge2}}
+                {kidAge3 == "" ? "" : "Child 3 age: "} {kidAge3 == "" ? "" : {kidAge3}}
+            <div>
+
+            </div>
+            <div>
+                About: {description}
+            </div>
+        </>
+    )
+}
+
 export function Matching() {
     return(
         <>
