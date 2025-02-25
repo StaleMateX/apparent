e6af7178fefdfb41d0770ed285658580975651ea
BrandiKeylor <u1307295@umail.utah.edu>
2025-02-25 06:08:03 -0700

    Broken profile page because I changed the userProfile model

--- apparent_backend/register/serializers.py
+++ apparent_backend/register/serializers.py
@@ -3,23 +3,27 @@ from django.contrib.auth.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    #Todo: refactor the password and email into models.py
     password = serializers.CharField(write_only=True)
     email = serializers.EmailField()  # Ensures valid email format
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-
+        fields = ["user", "email", "first_name", "last_name"]
+    #Todo refactor the below methods into models.py
     def validate_email(self, value):
         if User.objects.filter(email__iexact=value).exists():
             raise serializers.ValidationError("This email is already registered.")
         return value
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = User(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
-
+        # Setting the password this way hashes it!
+        user.set_password(validated_data['password'])
+        user.save()
         return user
--- apparent_backend/userProfile/admin.py
+++ apparent_backend/userProfile/admin.py
@@ -3,7 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Profile
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display= ("user", "uID", "first_name", "last_name")
 
-admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Profile)
