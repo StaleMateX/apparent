29ec497c65f1e8757d4eb7a0da278a4801f601d9
BrandiKeylor <u1307295@umail.utah.edu>
2025-02-26 12:28:35 -0700

    Restructured the UserProfileInfo page. Remember to disable any editing capabilities when other users are viewing.

--- apparent_frontend/src/components/UserProfileInfo.jsx
+++ apparent_frontend/src/components/UserProfileInfo.jsx
@@ -1,42 +1,63 @@
 import "./UserProfileInfo.css";
 import { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
 import Form from "react-bootstrap/Form";
 import { ProfilePicture } from "../components/ProfilePicture";
 
 export function UserProfileInfo({ firstName, lastName }) {
   return (
-    <>
-      <Stack direction="horizontal" gap={3}>
-        <div className="picture-container mt-2">
+    <Container>
+      <Row className="align-items-center">
+        {/* <Stack direction="horizontal" gap={3}> */}
+        <Col /* className="picture-container mt-2" */>
           <ProfilePicture
-            className="profile-picture"
+            /* className="profile-picture" */
             firstName={firstName}
             lastName={lastName}
           />
-        </div>
-        <div className="p-2 info-container">
-          <p className="info-text">{"College Name"}</p>
-          <p className="info-text">{"City, State"}</p>
-          <p className="info-text">Background Check: {"Status Unknown"}</p>
-        </div>
-        <div className="p-2 info-container">
-          <p className="info-text">Hobbies: {"Not specified"}</p>
-          <p className="info-text">Children: {"Not specified"}</p>
-        </div>
-      </Stack>
-      <Form className="d-flex centered-label">
-        <Form.Group controlId="aboutMeTextarea">
-          <Form.Label className="centered-label pt-2">About Me</Form.Label>
-          <Form.Control
-            as="textarea"
-            className="bg-color-light hide-scrollbar about-me-form mt-0"
-            rows={2}
-            value={""}
-            readOnly
-          />
-        </Form.Group>
-      </Form>
-    </>
+        </Col>
+        <Col className="d-flex flex-column">
+          <Row>
+            <Col className="d-flex justify-content-end">
+              <Button variant="outline-primary">Edit Profile</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="p-2 info-container">
+              <p className="info-text">{"College Name"}</p>
+              <p className="info-text">{"City, State"}</p>
+              <p className="info-text">Background Check: {"Status Unknown"}</p>
+            </Col>
+            <Col className="p-2 info-container">
+              <p className="info-text">Hobbies: {"Not specified"}</p>
+              <p className="info-text">Children: {"Not specified"}</p>
+            </Col>
+          </Row>
+        </Col>
+        {/* </Stack> */}
+      </Row>
+      <Row className="mt-3">
+        <Form
+          className="text-center w-100" /* className="d-flex centered-label" */
+        >
+          <Form.Group controlId="aboutMeTextarea">
+            <Form.Label /* className="centered-label pt-2" */>
+              About Me
+            </Form.Label>
+            <Form.Control
+              as="textarea"
+              className="bg-color-light hide-scrollbar about-me-form mt-0 w-100"
+              rows={2}
+              value={""} /*  TODO: Probably put a handler here */
+              placeholder={"What do you look for in parent friends?"}
+            />
+          </Form.Group>
+        </Form>
+      </Row>
+    </Container>
   );
 }
