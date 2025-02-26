553aa05c6025c0e80489c5761f0958077d771c15
BrandiKeylor <u1307295@umail.utah.edu>
2025-02-26 05:39:59 -0700

    Changed the register to include first and last name fields.

--- apparent_backend/register/serializers.py
+++ apparent_backend/register/serializers.py
@@ -3,27 +3,27 @@ from django.contrib.auth.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    #Todo: refactor the password and email into models.py
     password = serializers.CharField(write_only=True)
     email = serializers.EmailField()  # Ensures valid email format
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
 
     class Meta:
         model = User
-        fields = ["user", "email", "first_name", "last_name"]
-    #Todo refactor the below methods into models.py
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']
+
     def validate_email(self, value):
         if User.objects.filter(email__iexact=value).exists():
             raise serializers.ValidationError("This email is already registered.")
         return value
 
     def create(self, validated_data):
-        user = User(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            password=validated_data['password'],
             first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            last_name=validated_data['last_name'],
         )
-        # Setting the password this way hashes it!
-        user.set_password(validated_data['password'])
-        user.save()
+
         return user
